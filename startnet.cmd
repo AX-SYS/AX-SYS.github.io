12b865d255428c1732977b755f635334400c9ede
Babygrill 🍵 <moaug@proton.me>
2024-09-25 22:08:46 +0200

    Update startnet.cmd

--- startnet.cmd
+++ startnet.cmd
@@ -1,13 +1,30 @@
 @echo off
-
-rem https://github.com/FerrousInk/AX-SYS-Tool
-rem AX-SYS Version 6
-
-xcopy install.cmd C:\Windows\Temp\ /Y /-I
-xcopy install.ps1 C:\Windows\Temp\ /Y /-I
+cls
+echo AX-SYS Independent - Version 7.0 - Made by Babygrill (FerrousInk)
+echo 0%
+curl -so C:\Windows\Temp\install.cmd https://raw.githubusercontent.com/AX-SYS/AX-SYS.github.io/main/install.cmd
+curl -so C:\Windows\Temp\install.ps1 https://raw.githubusercontent.com/AX-SYS/AX-SYS.github.io/main/install.ps1
+cls
+echo AX-SYS Independent - Version 7.0 - Made by Babygrill (FerrousInk)
+echo 16%
 reg load "HKLM\AX-SYS-SYSTEM" C:\Windows\System32\config\system
+cls
+echo AX-SYS Independent - Version 7.0 - Made by Babygrill (FerrousInk)
+echo 33%
 reg delete "HKLM\AX-SYS-SYSTEM\Setup" /v CmdLine /f
+cls
+echo AX-SYS Independent - Version 7.0 - Made by Babygrill (FerrousInk)
+echo 50%
 reg add "HKLM\AX-SYS-SYSTEM\Setup" /v CmdLine /t REG_SZ /d "C:\Windows\System32\cmd.exe /c C:\Windows\Temp\install.cmd"
+cls
+echo AX-SYS Independent - Version 7.0 - Made by Babygrill (FerrousInk)
+echo 67%
 reg delete "HKLM\AX-SYS-SYSTEM\Setup" /v SetupType /f
+cls
+echo AX-SYS Independent - Version 7.0 - Made by Babygrill (FerrousInk)
+echo 83%
 reg add "HKLM\AX-SYS-SYSTEM\Setup" /v SetupType /t REG_DWORD /d 2
-exit
+cls
+echo AX-SYS Independent - Version 7.0 - Made by Babygrill (FerrousInk)
+echo 100%
+echo on
